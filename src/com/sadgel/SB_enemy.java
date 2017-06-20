879c5f99debe9f0ada8320aad8b9822a9ccc6dcc
SadGel <gel78@mail.ru>
2017-06-20 15:47:57 +0400

    v1

--- src/com/sadgel/SB_enemy.java
+++ src/com/sadgel/SB_enemy.java
@@ -40,42 +40,158 @@ public class SB_enemy implements Runnable {
         }
     }
 
-    private static HashSet isInjured(Bat_Field bf) {
-        HashSet rez = new HashSet();
-        boolean inj = false;
-        //HashSet[] ships = {};
+    private static Bat_cell[] getInjured(Bat_Field bf) {
 
-        //Iterator<Set> iterator = new bf.ships.iterator();
-        System.out.println(bf.ships.size());
+        Bat_cell[] rez = new Bat_cell[0];
+        boolean inj;
+        Set BitDecks;
 
         Iterator iterator = bf.ships.iterator();
         while (iterator.hasNext()) {
+            BitDecks = new HashSet();
             Set ship = (Set) iterator.next();
             inj = false;
             Bat_cell[] shipAr =  (Bat_cell[]) ship.toArray(new Bat_cell[ship.size()]);
             boolean tempPressed = shipAr[0].pressed;
-
             for (Bat_cell elem :shipAr) {
 
                 if (elem.pressed != tempPressed) {
                     inj = true;
                 }
 
+                if (elem.pressed) {
+                    BitDecks.add(elem);
+                }
+
             }
 
-            if (inj) {
-                System.out.println(shipAr.length);
+            if (inj) { //Есть раненый
+                int x,y,dx,dy;
+                Bat_cell[] BitDecksAr =  (Bat_cell[]) BitDecks.toArray(new Bat_cell[BitDecks.size()]);
+                System.out.println(BitDecksAr.length);
+                Set CellForBit = new HashSet();
+
+                if (BitDecksAr.length == 1) { // ранена одна палуба
+
+                    x = BitDecksAr[0].getX();
+                    y = BitDecksAr[0].getY();
+
+
+                    dx = x;dy=y-1; // клетка 1
+
+                    if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                        if (!bf.arOur[dx][dy].pressed) {
+                            CellForBit.add(bf.arOur[dx][dy]);
+                        }
+                    }
+
+                    dx = x+1;dy=y; // клетка 2
+
+                    if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                        if (!bf.arOur[dx][dy].pressed) {
+                            CellForBit.add(bf.arOur[dx][dy]);
+                        }
+                    }
+
+                    dx = x;dy=y+1; // клетка 3
+
+                    if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                        if (!bf.arOur[dx][dy].pressed) {
+                            CellForBit.add(bf.arOur[dx][dy]);
+                        }
+                    }
+
+                    dx = x-1;dy=y; // клетка 4
+
+                    if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                        if (!bf.arOur[dx][dy].pressed) {
+                            CellForBit.add(bf.arOur[dx][dy]);
+                        }
+                    }
+
+                    Bat_cell[] CellForBitAr =  (Bat_cell[]) CellForBit.toArray(new Bat_cell[CellForBit.size()]);
+
+                    return CellForBitAr;
+
+
+                }else { //Ранено больше одной палубы
+
+                    int a1 = BitDecksAr[0].getX();
+                    int a2 = BitDecksAr[1].getX();
+                    boolean vert = (a1 == a2);
+
+                    if (vert) { //Карабль вертикальный
+                        dx = BitDecksAr[0].getX();
+                        int dymax = 0;
+                        int dymin = 11;
+
+                        for (Bat_cell elem:BitDecksAr) {
+                           int dyTemp = elem.getY();
+                           if (dyTemp >= dymax) dymax = dyTemp;
+                           if (dyTemp <= dymin) dymin = dyTemp;
+                        }
+
+                        dy=dymin-1; // клетка 1
+
+                        if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                            if (!bf.arOur[dx][dy].pressed) {
+                                CellForBit.add(bf.arOur[dx][dy]);
+                            }
+                        }
+
+                        dy=dymax+1; // клетка 2
+
+                        if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                            if (!bf.arOur[dx][dy].pressed) {
+                                CellForBit.add(bf.arOur[dx][dy]);
+                            }
+                        }
+
+                        Bat_cell[] CellForBitAr =  (Bat_cell[]) CellForBit.toArray(new Bat_cell[CellForBit.size()]);
+
+                        return CellForBitAr;
+
+                    }else{ //Карабль горизонтальный
+                        dy = BitDecksAr[0].getY();
+                        int dxmax = 0;
+                        int dxmin = 11;
+
+                        for (Bat_cell elem:BitDecksAr) {
+                            int dxTemp = elem.getX();
+                            if (dxTemp >= dxmax) dxmax = dxTemp;
+                            if (dxTemp <= dxmin) dxmin = dxTemp;
+                        }
+
+                        dx=dxmin-1; // клетка 1
+
+                        if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                            if (!bf.arOur[dx][dy].pressed) {
+                                CellForBit.add(bf.arOur[dx][dy]);
+                            }
+                        }
+
+                        dx=dxmax+1; // клетка 2
+
+                        if ((dx <= 10) & (dy <= 10) & (dy >= 1) & (dx >= 1)){
+                            if (!bf.arOur[dx][dy].pressed) {
+                                CellForBit.add(bf.arOur[dx][dy]);
+                            }
+                        }
+
+                        Bat_cell[] CellForBitAr =  (Bat_cell[]) CellForBit.toArray(new Bat_cell[CellForBit.size()]);
+
+                        return CellForBitAr;
+
+                    }
+
+
+
+                }
+
+
             }
 
-            //System.out.println(shipAr.length);
 
-            //Iterator iteratorShip = ship.iterator();
-            //while (iteratorShip.hasNext()) {
-
-            //    Bat_cell bt = (Bat_cell) iteratorShip.next();
-
-
-            //}
         }
 
 
@@ -83,12 +199,34 @@ public class SB_enemy implements Runnable {
 
     }
 
+    private static  Bat_cell returnCellForBit(Bat_cell[] CellsForBitAr) {
+        Bat_cell rez;
+
+        Random random = new Random();
+
+        rez = CellsForBitAr[random.nextInt(CellsForBitAr.length)];
+
+        return rez;
+
+    }
 
     public static int[] AI_enemy(Bat_Field bf) {
 
-        isInjured(bf);
-
+        Bat_cell[] CellsForBitAr;
+        Bat_cell CellForBit;
         int[] rez = new int[2];
+
+        CellsForBitAr = getInjured(bf);
+
+        if (CellsForBitAr.length > 0) {
+
+            CellForBit = returnCellForBit(CellsForBitAr);
+            rez[0] = CellForBit.getX();
+            rez[1] = CellForBit.getY();
+            return rez;
+        }
+
+
         Random random = new Random();
 
 
